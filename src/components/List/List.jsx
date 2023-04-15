4e9be00fb0c11c4eaca150703a8300063186d95a
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-15 14:12:18 +0530

    Folder Structure-02

---
+++ src/components/List/List.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import ReactDOM from "react-dom";
+
+const List = ()=>{
+    return(
+        <h1>List</h1>
+    );
+}
+
+export default List;
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import ReactDOM from "react-dom";
+
+const Map = ()=>{
+    return(
+        <h1>Map</h1>
+    );
+}
+
+export default Map;
