c6ec47d988138dfebe6ce95d7f16fc1883320c22
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-16 20:51:24 +0530

    map.jsx 01

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOutlinedIcon from "@material-ui/icons/LocationOutlined";
+import Rating from "@material-ui/lab";
 
-import ReactDOM from "react-dom";
+import useStyles  from "./styles";
+const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600ox)')
 
-const Map = ()=>{
-    return(
-        <h1>Map</h1>
-    );
-}
+  return(
+
+  );
+};
 
 export default Map;
