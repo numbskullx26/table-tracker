8d8c99aeffcd8786e4d4be934b0a61deee650ff5
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-16 21:45:17 +0530

    List.jsx 01

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from "@material-ui/core";
-import LocationOutlinedIcon from "@material-ui/icons/LocationOutlined";
-import Rating from "@material-ui/lab";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab/Rating";
 
 import useStyles from "./styles";
 const Map = () => {
   const classes = useStyles();
-  const isMobile = useMediaQuery("(min-width:600ox)");
+  const isMobile = useMediaQuery("(min-width:600px)");
 
   const coordinates = { lat: 0, lng: 0 };
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: " " }}
+        bootstrapURLKeys={{ key: "AIzaSyBFIh91lzDwqRwMGVqdHseLznDQJ-3v7Ks" }}
         defaultCenter={coordinates}
         center={coordinates}
         defaultZoom={14}
         margin={(50, 50, 50, 50)}
         options={" "}
-        onChange={" "}
         onChildClick={" "}
       ></GoogleMapReact>
     </div>
