f5e0520575986eb3e1d4d09fe8e58632df0db574
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-17 18:16:07 +0530

    PlaceDetails.jsx 02

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -13,13 +13,30 @@ import {
 
 import LocationOnIcon from "@material-ui/icons/LocationOn";
 import PhoneIcon from "@material-ui/icons/Phone";
-import Rating from "@material-ui/icons/Rating";
+// import Rating from "@material-ui/icons/Rating";
 
 import useStyles from "./styles";
 
 const PlaceDetails = ({ place }) => {
-  console.log(place);
-  return <h1>{place.name}</h1>;
+  const classes = useStyles();
+  return (
+    <Card elevation={6}>
+      <CardMedia
+        style={{ height: 350 }}
+        image={
+          place.photo
+            ? place.photo.images.large.url
+            : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"
+        }
+        title={place.name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h6">
+          {place.name}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default PlaceDetails;
