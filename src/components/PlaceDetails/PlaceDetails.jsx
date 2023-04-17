73783dbbd658b793d0b7fdd7e709876a3eba328e
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-17 18:01:40 +0530

    PlaceDetails.jsx 01

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 
+import {
+  Box,
+  Typography,
+  Button,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Chip,
+} from "@material-ui/core";
+
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import PhoneIcon from "@material-ui/icons/Phone";
+import Rating from "@material-ui/icons/Rating";
+
+import useStyles from "./styles";
+
 const PlaceDetails = ({ place }) => {
   console.log(place);
   return <h1>{place.name}</h1>;
