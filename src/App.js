87a715a939595ee9ed5dc2b9a8b8646211b6352a
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-17 13:27:42 +0530

    Solved the sw null error

--- src/App.js
+++ src/App.js
@@ -10,26 +10,33 @@ import { CssBaseline, Grid } from "@material-ui/core";
 const App = () => {
   const [places, setPlaces] = useState([]);
 
-  const [coordinates, setCooridnates] = useState({});
-  const [bounds, setBounds] = useState(null);
-  useEffect(() => {
-    getPlacesData().then((data) => {
-      console.log(data);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({});
 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    getPlacesData(bounds.sw, bounds.ne).then((data) => {
       setPlaces(data);
     });
-  }, []);
+  }, [coordinates, bounds]);
   return (
     <>
       <CssBaseline />
       <Header />
       <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
-          <List />
+          <List places={places} />
         </Grid>
         <Grid item xs={12} md={8}>
           <Map
-            setCooridnates={setCooridnates}
+            setCoordinates={setCoordinates}
             setBounds={setBounds}
             coordinates={coordinates}
           />
