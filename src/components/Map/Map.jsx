01d83cdc4f304fd158f38454078dfd5262ff29d7
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-18 12:17:52 +0530

    MapStyling Completed

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -3,7 +3,7 @@ import GoogleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from "@material-ui/core";
 import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 import Rating from "@material-ui/lab/Rating";
-
+import mapStyles from "./mapStyles";
 import useStyles from "./styles";
 const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
   const classes = useStyles();
